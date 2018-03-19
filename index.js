cdfe48ab680ea233227d5572aa137b0a422cabad
timurdua <hendrastevian2006@gmail.com>
2018-03-19 16:21:59 +0700

    add map

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ $('html,body').animate({
 	scrollTop: elementTujuan.offset().top - 50
 }, 500, 'swing');
 });
+
+// parallax
+$(window).scroll(function(){
+	var wscroll = $(this).scrollTop();
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px,'+ wscroll/2 +'%)'
+	});
+$('.jumbotron p').css({
+		'transform' : 'translate( 0px,'+ wscroll/4 +'%)'
+	});
+$('.jumbotron h4').css({
+		'transform' : 'translate( 0px, '+ wscroll/4 +'%)'
+	});
+});
