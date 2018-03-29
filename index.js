3ec55e012ce73ff96de7cbf4c7008162dd4ba931
timurdua <hendrastevian2006@gmail.com>
2018-03-29 11:03:01 +0700

    update jumbotron and paralax

--- index.js
+++ index.js
@@ -12,16 +12,46 @@ $('html,body').animate({
 // parallax
 $(window).scroll(function(){
 	var wscroll = $(this).scrollTop();
+<<<<<<< HEAD
+	// jumbotron
 	$('.jumbotron h1').css({
 		'transform' : 'translate(0px,'+ wscroll/2 +'%)'
 	});
+	$('.jumbotron p').css({
+		'transform' : 'translate( 0px,'+ wscroll/4 +'%)'
+	});
+	$('.jumbotron h4').css({
+		'transform' : 'translate( 0px, '+ wscroll*1.5 +'%)'
+	});
+	$('.jumbotron a').css({
+		'transform' : 'translate( 0px, '+ wscroll +'%)'
+	});
+	// video
+	if(wscroll > $('.video').offset().top - 300) {
+		$('.video .thumbnail').each(function()) {
+			setTimeout(function() {
+
+			}, 2000)
+		}
+		// $('.video .thumbnail').addClass('muncul');
+	}
+
+
+
+
+=======
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px,'+ wscroll/4 +'%)'
+	});
 $('.jumbotron p').css({
-		'transform' : 'translate( 0px,'+ wscroll/3 +'%)'
+		'transform' : 'translate( 0px,'+ wscroll/8 +'%)'
 	});
 $('.jumbotron h4').css({
-		'transform' : 'translate( 0px, '+ wscroll*1.5 +'%)'
+		'transform' : 'translate( 0px, '+ wscroll +'%)'
 	});
 $('.link a').css({
 		'transform' : 'translate( 0px, '+ wscroll/2 +'%)'
 	});
+>>>>>>> eed06365c5a199e055293d65d50925a730115d84
+
 });
